8b41eb182647596de895f3a1608929055ff600b9
kawan <kawannajjar@gmail.com>
2025-08-20 15:33:52 +0330

    added gerenal templates

--- website/views.py
+++ website/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-
 def index_view(request):
     return render(request, 'website/index.html')
 def about_view(request):
